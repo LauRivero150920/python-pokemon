9d854e3880ca577cd979174534af46cbe084b442
LauRivero <A01274144@tec.mx>
2023-05-27 22:18:03 -0600

    double dmage to and from added

--- pokemon.py
+++ pokemon.py
@@ -24,13 +24,30 @@ print("You selected", selected_pokemon)
 
 pokemon_API_info = retrieve_response('https://pokeapi.co/api/v2/pokemon/', selected_pokemon)
 
+pokemon_type = pokemon_API_info['types'][0]['type']['name']
+
 #* Information Needed
 # Pokemon Image
 image_link = pokemon_API_info['sprites']['versions']['generation-vii']['ultra-sun-ultra-moon']['front_default']
 
-#! Double damage from
+# Double damage from
+db_dmg = retrieve_response('https://pokeapi.co/api/v2/type/', pokemon_type)
 
-#! Double damage to
+db_dmg_from_array = []
+db_dmg_from = db_dmg['damage_relations']['double_damage_from']
+
+db_dmg_from_array = [0 for i in range(len(db_dmg_from))] 
+for i in range(len(db_dmg_from)):
+    db_dmg_from_array[i] = db_dmg_from[i]['name']
+
+# Double damage to
+db_dmg_to_array = []
+db_dmg_to = db_dmg['damage_relations']['double_damage_to']
+
+db_dmg_to_array = [0 for i in range(len(db_dmg_to))] 
+for i in range(len(db_dmg_to)):
+    db_dmg_to_array[i] = db_dmg_to[i]['name']
+
 # Evolutions
 pokemon_species_info = retrieve_response('https://pokeapi.co/api/v2/pokemon-species/', selected_pokemon)
 evolution_chain_url = pokemon_species_info['evolution_chain']['url']
