091f50974cf13e7708350130fd895eafd3753057
LauRivero <A01274144@tec.mx>
2023-05-29 21:39:10 -0600

    Added data replacement for html

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,32 @@
 import requests 
 import json
+import os
+import re
+from bs4 import BeautifulSoup as bs
 
 def retrieve_response(url, selected_pokemon):
     response_pokemon = requests.get(url + selected_pokemon)
     data = response_pokemon.text
     output = json.loads(data)
     return output
+
+def update_html(id, poke_data):
+    with open("html/" + selected_pokemon + ".html") as poke_html:
+        soup = bs(poke_html, "html.parser")
+    target = soup.find(id=id)
+    target.find(string=re.compile(id)).replace_with(str(poke_data).capitalize())
+
+    with open("html/" + selected_pokemon + ".html", "wb") as poke_html:
+        poke_html.write(soup.prettify("utf-8"))
+
+def arrange_array(array):
+    out_string = ""
+    for i in range(len(array)):
+        if(i != len(array) - 1):
+            out_string += array[i] + ", "
+        else:
+            out_string += array[i]
+    return out_string
 
 print("**** Pokemon list ****")
 pokemon = ['bulbasaur', 'charmander', 'squirtle', 'pichu']
@@ -112,3 +133,38 @@ with open('css/style.css', 'w') as poke_css:
 
 for line in base_file:
     poke_html.write(line)
+
+with open("html/" + selected_pokemon + ".html", "r") as poke_html:
+    poke_html_data = poke_html.readlines()
+
+poke_html_data[14] = f"\t\t<img src=\"{image_link}\" alt=\"{selected_pokemon}\" id=\"poke_image\">"
+
+with open("html/" + selected_pokemon + ".html", "w") as poke_html:
+    poke_html.writelines(poke_html_data)
+
+poke_html.close()
+poke_css.close()
+base_file.close()
+
+update_html("pokemon_name", selected_pokemon)
+
+db_dmg_from = arrange_array(db_dmg_from_array)
+update_html("double_from", db_dmg_from)
+
+db_dmg_to = arrange_array(db_dmg_to_array)
+update_html("double_to", db_dmg_to)
+
+update_html("poke_type", pokemon_type)
+
+update_html("evol_1", evolution_1)
+update_html("evol_2", evolution_2)
+
+update_html("poke_hp", poke_hp)
+update_html("poke_attack", poke_attack)
+update_html("poke_defense", poke_defense)
+update_html("poke_bexp", poke_base_exp)
+
+update_html("name_ability_1", abilities_names[0])
+update_html("poke_ability_1", abilities_array[0])
+update_html("name_ability_2", abilities_names[1])
+update_html("poke_ability_2", abilities_array[1])
